f6c8958106d6379a9c97675a774a09acf2637c5f
beckytieman45 <beckytieman@gmail.com>
2022-01-19 22:10:19 -0700

    Co-authored-by: Cardon-Erika <Cardon-Erika@users.noreply.github.com> Co-authored-by: prattsbyui <prattsbyui@users.noreply.github.com> Co-authored-by: Meg <meganlynn012@users.noreply.github.com> Co-authored-by: Bekiboo <Bekiboo@users.noreply.github.com> Co-authored-by: CaraAgard <CaraAgard@users.noreply.github.com>

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,50 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
-router.get('/', (req, res, next) => {
+const p = path.join(path.dirname(require.main.filename), 'data', 'data.json');
+let datas = [];
+
+router.post('/search', (req, res, next) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+        [];
+    } else {
+        datas = JSON.parse(fileContent);
+    }
+  });
+
+  for (let x = datas.length - 1; x >= 0; x++) {
+    if ((datas[x].name).includes(req.body.result)) {
+      datas.splice(x, 1);
+    }
+  }
+
   res.render('pages/ta03', {
     title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    data: datas,
+    path: '/ta03',
+  });
+
+});
+
+router.get('/', (req, res, next) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+        [];
+    } else {
+        datas = JSON.parse(fileContent);
+    }
+});
+  res.render('pages/ta03', {
+    title: 'Team Activity 03',
+    data: datas,
+    path: '/ta03',
   });
 });
+
+
 
 module.exports = router;
